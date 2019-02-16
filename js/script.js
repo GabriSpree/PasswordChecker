1071e3de58412620a2f7919b220889046e329ddf
Gabriele <Gabri03@users.noreply.github.com>
2019-02-16 14:43:45 +0100

    Adapted to new folder locations

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function enter(key) {
+    var keyValid = new Boolean();
+    if (key == "") {
+        keyValid = false;
+    } else {
+        keyValid = true;
+    }
+
+    if (keyValid == true) {} else if (keyValid == false) {}
+}
