8ce2a90ac305f3d2dbf93a1d3b99b28e565026a5
Gabriele <Gabri03@users.noreply.github.com>
2019-02-16 18:03:09 +0100

    Updat script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,13 @@ function enter(key) {
 function showIdLogin(hasToShowPopup) {
   if (hasToShowPopup) {
     document.getElementById("popup").style.display = "block";
+    document.getElementById("idlogin-part0").innerHTML = "IDLogger is opened >";
+    document.getElementById("idlogin-part1").innerHTML = "";
+    document.getElementById("link-idlogin").style.display = "none";
   } else if (!hasToShowPopup) {
     document.getElementById("popup").style.display = "none";
+    document.getElementById("idlogin-part0").innerHTML = "If you have to login with your ID instead of your ContentID, ";
+    document.getElementById("idlogin-part1").innerHTML = " to open the IDLogger."
+    document.getElementById("link-idlogin").style.display = "inline";
   }
 }
