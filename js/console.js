5245d55994f535e7f58d063cd7fe17f96267d2d6
GabriSpree <39029126+GabriSpree@users.noreply.github.com>
2019-02-18 19:39:59 +0100

    Update console.js

--- js/console.js
+++ js/console.js
@@ -1,1 +1,29 @@
 // Not a blank document and console script, sorry GitHub for trolling:)
+var userInput = document.getElementById("userInput");
+var passInput = document.getElementById("passInput");
+
+const $userInput = document.querySelector('#userInput');
+const $passInput = document.querySelector('#passInput');
+
+const userHandler = function(inputElmnt) {
+  var userValue = inputElmnt.target.value;
+  if (userValue.contains("admi")) {
+    userInput.maxlength = "8";
+  } else {
+    userInput.maxlength = "4"; 
+  }
+}
+
+const passHandler = function(inputElmnt) {
+  var passValue = inputElmnt.target.value;
+  if (passValue.contains("000a") {
+    passInput.maxlength = "9";
+  } else {
+    passInput.maxlength = "4";
+  }
+}
+
+$userInput.addEventListener('input', userHandler);
+$userInput.addEventListener('propertychange', userHandler);
+$passInput.addEventListener('input', passHandler);
+$passInput.addEventListener('propertychange', passHandler);
