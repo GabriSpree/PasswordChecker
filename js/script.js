0cc1bb92d0c4217f3d451c6673ba905f9c54e401
Gabriele <Gabri03@users.noreply.github.com>
2019-03-02 22:36:18 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function enter(key) {
         keyValid = true;
     }
 
-    if (keyValid == true) {} else if (keyValid == false) {}
+    if (keyValid) {} else if (!keyValid) {}
 }
 
 // For *IdLogin entering program
@@ -24,16 +24,51 @@ function showIdLogin(hasToShowPopup) {
   }
 }
 
+function makeKeyId(hasToSet, hasToDestroy) {
+  var sKeyInput = document.getElementById("sKeyInput");
+  var sKeyInput_p = document.getElementById("sKeyInput_p");
+  var sKeyShower = document.getElementById("sKeyShower");
+
+  var stext = "";
+  var stext2 = "";
+  var spossible = "AbCdEfGhIjKlMnOpQrStUvWxYzaBcDeFgHiJkLmNoPqRsTuVwXyZ7946281305";
+
+  if (hasToSet) {
+
+    for (var r0 = 0; r0 < 120; r0++) {
+      stext += spossible.charAt(Math.floor(Math.random() * spossible.length));
+    }
+
+    sKeyInput.value = stext;
+    sKeyInput_p.value = stext;
+
+    for (var r0 = 0; r0 < 1000; r0++) {
+      stext2 += spossible.charAt(Math.floor(Math.random() * spossible.length));
+    }
+
+    sKeyShower.innerHTML = stext2;
+
+  } else if (hasToDestroy) {
+
+    sKeyInput.value = "";
+    sKeyInput_p.value = "";
+    sKeyShower.innerHTML = "";
+
+  }
+}
+
+setInterval(function(){makeKeyId(false,true);makeKeyId(true,false)}, 10);
+
 dragElement(document.getElementById("popup"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id)) {
+  if (document.getElementById("popup")) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "-sub").onmousedown = dragMouseDown;
+    document.getElementById("popup" + "-sub").onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
+    elmnt.onmousedown = dragMouseDown(elmnt);
   }
 
   function dragMouseDown(e) {
@@ -42,9 +77,9 @@ function dragElement(elmnt) {
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
+    document.onmouseup = closeDragElement(elmnt);
     // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
+    document.onmousemove = elementDrag(elmnt);
   }
 
   function elementDrag(e) {
